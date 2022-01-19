4cde742259403c394bd6d661bb4339a72d9673d6
AliaksandrMoroz <89323365+AliaksandrMoroz@users.noreply.github.com>
2022-01-19 16:33:35 +0300

    add Maven and Controllers

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class mainController {
+public class MainController {
 
     @FXML
     private ResourceBundle resources;
