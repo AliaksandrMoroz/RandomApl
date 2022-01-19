468a1d4049b6ecf353227d1df1f7471db6debda9
AliaksandrMoroz <89323365+AliaksandrMoroz@users.noreply.github.com>
2022-01-19 16:14:10 +0300

    add Project

---
+++ src/sample/controllers/mainController.java
@@ -0,0 +1,57 @@
+package sample.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class mainController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button addStudentButton;
+
+    @FXML
+    private Button deleteStudentButton;
+
+    @FXML
+    private Button randomStudentVsStudentButton;
+
+    @FXML
+    private Button setStudentButton;
+
+    @FXML
+    private Button showStudentButton;
+
+    @FXML
+    void initialize() {
+
+        addStudentButton.setOnAction(event -> {
+
+        });
+
+        deleteStudentButton.setOnAction(event -> {
+
+        });
+
+        setStudentButton.setOnAction(event -> {
+
+        });
+
+        showStudentButton.setOnAction(event -> {
+
+        });
+
+        randomStudentVsStudentButton.setOnAction(event -> {
+
+        });
+
+
+    }
+
+}
